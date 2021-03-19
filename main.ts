241809f54ebbe896ec191d5de6aa0119d9426383
Linus Rundberg Streuli <schizoakustik@schizoakustik.se>
2021-03-19 11:59:13 +0100

    Update main.ts, test.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,30 @@
+let snake = game.createSprite(2, 2)
+let food = game.createSprite(0, 0)
+food.off()
+
+function read_x() {
+    if (microboy.readJoystick(Axis.X) < 350) {
+        if (snake.get(LedSpriteProperty.X) == 4) { game.gameOver() }
+        else { snake.changeXBy(1) }
+    }
+    else if (microboy.readJoystick(Axis.X) > 750) {
+        if (snake.get(LedSpriteProperty.X) == 0) { game.gameOver() }
+        snake.changeXBy(-1)
+    }
+}
+
+function read_y() {
+    if (microboy.readJoystick(Axis.Y) < 350) {
+        if (snake.get(LedSpriteProperty.Y) == 4) { game.gameOver() }
+        else { snake.changeYBy(1) }
+    }
+    else if (microboy.readJoystick(Axis.Y) > 750) {
+        if (snake.get(LedSpriteProperty.Y) == 0) { game.gameOver() }
+        snake.changeYBy(-1)
+    }
+}
+
 basic.forever(function () {
-	
+        read_x()
+        read_y()
 })
--- test.ts
+++ test.ts
@@ -1,1 +1,9 @@
 // tests go here; this will not be compiled when this package is used as an extension.
+
+basic.forever(function () {
+        read_x()
+        read_y()
+        pins.analogWritePin(AnalogPin.P1, 573)
+        while (input.buttonIsPressed(Button.A)) { pins.analogWritePin(AnalogPin.P1, 1023)}
+        while (input.buttonIsPressed(Button.B)) { pins.analogWritePin(AnalogPin.P1, 173)}
+})
